19f9ecaa9cd7f04a3c4be0cff804405553ed2e0b
Michael Schmid <michael@amazeelabs.com>
2016-03-28 14:33:18 +0200

    don't run in background

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 case "$1" in
     ssh-agent)
         # NOTE: openssh 6.9 introduces -D for running ssh-agent in the foreground.
-        exec sudo -u drupal /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} 2>&1
+        exec sudo -u drupal /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} -d 2>&1
         ;;
     *)
         exec $@;;
