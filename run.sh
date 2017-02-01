5bc4755adacc986c7410ea9328270b9825266a9b
Schnitzel <michael@amazee.com>
2017-02-01 07:07:58 -0600

    somehow windows-key-add got removed from run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,11 @@ case "$1" in
         rm -f ${SSH_AUTH_SOCK}
         exec sudo -u drupal /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} -d 2>&1
         ;;
+    windows-key-add)
+        cp -f $2 $2_
+        chmod 400 $2_
+        exec ssh-add $2_ 2>&1
+        ;;
     *)
         exec $@;;
 esac
